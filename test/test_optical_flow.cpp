598c879082bd06d4ea8a848b00bdad8048fc727d
Horizon1026 <lijiapeng_atlantis@163.com>
2024-04-20 17:57:03 +0800

    update code

--- test/test_optical_flow.cpp
+++ test/test_optical_flow.cpp
@@ -20,11 +20,6 @@
 using namespace SLAM_VISUALIZOR;
 
 #define DRAW_TRACKING_RESULT (1)
-#define DETECT_FEATURES_BY_OPENCV (0)
-
-#if OPENCV_IS_VALID
-#include "opencv2/opencv.hpp"
-#endif
 
 namespace {
     constexpr int32_t kMaxNumberOfFeaturesToTrack = 300;
@@ -37,23 +32,11 @@ std::string test_ref_image_file_name = "../example/optical_flow/ref_image.png";
 std::string test_cur_image_file_name = "../example/optical_flow/cur_image.png";
 
 void DetectFeatures(const GrayImage &image, std::vector<Vec2> &pixel_uv) {
-#if defined(OPENCV_IS_VALID) && DETECT_FEATURES_BY_OPENCV
-	cv::Mat cv_image(image.rows(), image.cols(), CV_8UC1, image.data());
-    std::vector<cv::Point2f> ref_corners;
-    cv::goodFeaturesToTrack(cv_image, ref_corners, kMaxNumberOfFeaturesToTrack, 0.01, 20);
-
-    pixel_uv.clear();
-    for (auto &item : ref_corners) {
-        pixel_uv.emplace_back(Vec2(item.x, item.y));
-    }
-#else
-    // Detect features.
     FEATURE_DETECTOR::FeaturePointDetector<FEATURE_DETECTOR::HarrisFeature> detector;
     detector.options().kMinFeatureDistance = 25;
     detector.feature().options().kHalfPatchSize = 1;
     detector.feature().options().kMinValidResponse = 40.0f;
     detector.DetectGoodFeatures(image, kMaxNumberOfFeaturesToTrack, pixel_uv);
-#endif
 }
 
 float TestOpticalFlowBasicKlt(int32_t pyramid_level, int32_t patch_size, uint8_t method) {
@@ -184,57 +167,8 @@ float TestOpticalFlowLssdKlt(int32_t pyramid_level, int32_t patch_size, uint8_t 
     return cost_time;
 }
 
-float TestOpencvLkOpticalFlow(int32_t pyramid_level, int32_t patch_size, uint8_t method) {
-    float cost_time = 0.0f;
-
-#if OPENCV_IS_VALID
-    cv::Mat cv_ref_image, cv_cur_image;
-    cv_ref_image = cv::imread(test_ref_image_file_name, 0);
-    cv_cur_image = cv::imread(test_cur_image_file_name, 0);
-
-    GrayImage ref_image(cv_ref_image.data, cv_ref_image.rows, cv_ref_image.cols);
-    GrayImage cur_image(cv_cur_image.data, cv_cur_image.rows, cv_cur_image.cols);
-
-    // Detect features.
-    std::vector<cv::Point2f> ref_corners;
-    std::vector<cv::Point2f> cur_corners;
-    std::vector<Vec2> ref_pixel_uv;
-    std::vector<Vec2> cur_pixel_uv;
-    DetectFeatures(ref_image, ref_pixel_uv);
-    for (const auto &item : ref_pixel_uv) {
-        ref_corners.emplace_back(cv::Point2f(item.x(), item.y()));
-    }
-
-    std::vector<uchar> status;
-    std::vector<float> errors;
-    cur_corners.reserve(ref_pixel_uv.size());
-    status.reserve(ref_pixel_uv.size());
-    errors.reserve(ref_pixel_uv.size());
-
-    cv::setNumThreads(1);
-    TickTock timer;
-    cv::calcOpticalFlowPyrLK(cv_ref_image, cv_cur_image, ref_corners, cur_corners, status, errors,
-        cv::Size(2 * patch_size + 1, 2 * patch_size + 1), pyramid_level - 1);
-    cost_time = timer.TockTickInMillisecond();
-
-#if DRAW_TRACKING_RESULT
-    for (const auto &item : cur_corners) {
-        cur_pixel_uv.emplace_back(Vec2(item.x, item.y));
-    }
-    // Visualizor::ShowImageWithDetectedFeatures("Opencv KLT : Feature before multi tracking", ref_image, ref_pixel_uv);
-    Visualizor::ShowImageWithTrackedFeatures("Opencv KLT : Feature after multi tracking", cur_image, ref_pixel_uv, cur_pixel_uv, status);
-    Visualizor::WaitKey(1);
-#endif // end of DRAW_TRACKING_RESULT
-#endif // end of OPENCV_IS_VALID
-
-    return cost_time;
-}
-
 int main(int argc, char **argv) {
-    float cost_time = TestOpencvLkOpticalFlow(kMaxPyramidLevel, kHalfPatchSize, static_cast<uint8_t>(kDefaultMethod));
-    ReportInfo("Opencv lk cost time " << cost_time << " ms.");
-
-    cost_time = TestOpticalFlowBasicKlt(kMaxPyramidLevel, kHalfPatchSize, static_cast<uint8_t>(kDefaultMethod));
+    float cost_time = TestOpticalFlowBasicKlt(kMaxPyramidLevel, kHalfPatchSize, static_cast<uint8_t>(kDefaultMethod));
     ReportInfo("Basic klt cost time " << cost_time << " ms.");
 
     cost_time = TestOpticalFlowAffineKlt(kMaxPyramidLevel, kHalfPatchSize, static_cast<uint8_t>(kDefaultMethod));
