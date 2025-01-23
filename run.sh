02d391480f7ef2227590d81848a8da03aeffbab1
Horizon1026 <lijiapeng_atlantis@163.com>
2025-01-24 00:02:19 +0800

    update code

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd build/
-# ./test_optical_flow
-# ./test_direct_method
-# ./test_descriptor_matcher_brief
+./test_optical_flow
+./test_direct_method
+./test_descriptor_matcher_brief
 ./test_descriptor_matcher_xfeat
 cd ..
